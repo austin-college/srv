c15064c4f62eafdeede9159a37a516e73a129b33
Lydia House <lhouse18@austincollege.edu>
2020-03-27 14:47:52 -0500

    added HTML file for splash page
    added static and template resource folders
    created MAV method to display splash page

--- src/main/java/srv/controllers/splash/SplashController.java
+++ src/main/java/srv/controllers/splash/SplashController.java
@@ -1,9 +1,14 @@
 
 package srv.controllers.splash;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * This is the algorithm that prepares the response. 
@@ -19,6 +24,23 @@ public class SplashController {
 	   @ResponseBody
 	   public String handleSplashRequest() {
 	      return "Hello Roo College";
+	   }
+	   
+	   /**
+	    * Splash action displays the splash page. See splash.html template
+	    * 
+	    * @param request
+	    * @param response
+	    * @return
+	    * 
+	    * @author lahouse
+	    */
+	   @GetMapping("/")
+	   public ModelAndView splash(HttpServletRequest request, HttpServletResponse response) {
+		   
+		   ModelAndView mav = new ModelAndView("splash");
+		   
+		   return mav;
 	   }
 }
 
