67809b8044a6a30b83aa09e4310ab80db583b012
HunterCouturier <hcouturier18@austincollege.edu>
2020-04-06 14:25:45 -0500

    Created a User domain object

--- src/main/java/srv/domain/User.java
+++ src/main/java/srv/domain/User.java
@@ -1,5 +1,27 @@
 package srv.domain;
-
+/**
+ * 
+ * @author hunter Couturier
+ *
+ */
 public class User {
+	private String userID;
+	private String password;
+	private Double totalHoursServed;
+	
+	/**
+	 * generic user constructor
+	 */
+	public User() {
+		
+	}
+	/**
+	 * Constructs a user with a set userID and Password
+	 * @param userID
+	 * @param password
+	 */
+	public User(String userID, String password) {
+		
+	}
 
 }
