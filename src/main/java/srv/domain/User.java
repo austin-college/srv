1832b7bd0e235a190f5c0ad0148df1031b78f9fe
HunterCouturier <hcouturier18@austincollege.edu>
2020-04-06 16:08:31 -0500

    Implemented user listAll

--- src/main/java/srv/domain/User.java
+++ src/main/java/srv/domain/User.java
@@ -1,4 +1,7 @@
 package srv.domain;
+
+import java.util.ArrayList;
+
 /**
  * 
  * @author hunter Couturier
@@ -8,12 +11,12 @@ public class User {
 	private String userID;
 	private String password;
 	private Double totalHoursServed;
-	
+
 	/**
 	 * generic user constructor
 	 */
 	public User() {
-		
+
 	}
 	/**
 	 * Constructs a user with a set userID and Password
@@ -21,7 +24,66 @@ public class User {
 	 * @param password
 	 */
 	public User(String userID, String password) {
+
+	}
+	/**
+	 * Takes an ArrayList, and from that list pulls
+	 * 3 users from it and returns them as a user array
+
+	 * @param userList
+	 * @return 3 Users from the list
+	 * 
+	 *
+	 */
+	public static User[] listAll(ArrayList<User> userList) {
+		User[] returnedArray = new  User[2];
+		//case if there is only one item in the array
+
+		if(userList.size() == 1) {
+			returnedArray[0] = userList.get(0);
+			returnedArray[1] = null;
+			returnedArray[2] = null;
+		}
+		//case if there are two items only
+		if(userList.size() == 2) {
+			returnedArray[0] = userList.get(0);
+			returnedArray[1] = userList.get(1);
+			returnedArray[2] = null;
+		}
+		//case if there are three items only
+		if(userList.size() == 3) {
+			returnedArray[0] = userList.get(0);
+			returnedArray[1] = userList.get(1);
+			returnedArray[2] = userList.get(2);
+		}
+
+		//case for any value about 3
+		if(userList.size() > 3) {
+			int used1 = -1;
+			int used2 = -1;
+			int used3 = -1;
+			/*
+			 * block of code here calls math.random
+			 * to find a random User to add to the list
+			 * it then checks the other two numbers to make
+			 * sure the user it entered to the list
+			 * is unique
+			 */
+			used1 = (int)(Math.random()*userList.size()+1);
+			returnedArray[0] = userList.get(used1);
+			 
+			do {
+			used2 = (int)(Math.random()*userList.size()+1);
+			} while(used1 == used2);
+			returnedArray[1] = userList.get(used2);
+			
+			do {
+				returnedArray[2] = userList.get(used3);
+			} while(used3 == used1 || used3 == used2);
+			return returnedArray;
+		}
 		
+		return null;
 	}
 
 }
