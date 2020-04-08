20e0e70420ac2670f239a7977e5edd2667b30e0e
AJPritchard <aj_pritch@hotmail.com>
2020-04-08 09:24:30 -0700

    Created Contact.java domain class

---
+++ src/main/java/srv/domain/Contact.java
@@ -0,0 +1,24 @@
+package srv.domain;
+ 
+/**
+ * @author AJ Pritchard (For Now)
+ * 
+ * This here is the ol' contact class
+ * 
+ * It is one of two things, we need to figure out if it is the controller for the contact page, 
+ * if so it needs to get moved to a controllers folder.
+ * I am pretty sure however that this is a domain object for our pets that control their contact information.
+ *
+ */
+public class Contact {
+
+	
+	private String phone_number;
+	private String name;
+	// private BoardMember connection; TODO
+	
+	public Contact(String name, String phone_number) { // TODO import connection
+		this.name = name;
+		this.phone_number = phone_number;
+	}
+}
