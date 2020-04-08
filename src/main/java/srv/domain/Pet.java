04b3e4323a4bf7d1b12f917bfa469f5f20798be7
AJPritchard <aj_pritch@hotmail.com>
2020-04-08 10:29:57 -0700

    Updated the pet page

--- src/main/java/srv/domain/Pet.java
+++ src/main/java/srv/domain/Pet.java
@@ -1,5 +1,8 @@
 package srv.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author AJ Pritchard (For Now)
  *
@@ -8,23 +11,36 @@ package srv.domain;
  */
 public class Pet {
 
+	private String name;
+	private String description;
 	private Contact petContact;
-	//private List<Event> events //TODO this pet should also hold onto its own events
-	
-	
-	public Pet(String name, String email, String phone_number) {
-		petContact = new Contact(name, phone_number, email);
+	private List<Event> events; // TODO this pet should also hold onto its own events
+
+	public Pet(String name, String description, String email, String phone_number) {
+		petContact = new Contact(phone_number, email);
+		this.name = name;
+		this.description = description;
+
+		events = new ArrayList<Event>();
 	}
-	
+
 	public String getEmail() {
 		return petContact.getEmail();
 	}
-	
+
 	public String getPhoneNumber() {
 		return petContact.getPhoneNumber();
 	}
-	
+
 	public String getName() {
-		return petContact.getName();
+		return name;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void addEvent(Event newEvent) {
+		events.add(newEvent);
 	}
 }
