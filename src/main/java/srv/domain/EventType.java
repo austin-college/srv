80079317e9806c3d2fba0992a033495daca9bf76
AJPritchard <aj_pritch@hotmail.com>
2020-04-08 10:22:13 -0700

    EventType.java added

---
+++ src/main/java/srv/domain/EventType.java
@@ -0,0 +1,20 @@
+package srv.domain;
+
+/**
+ * 
+ * 
+ * @author AJ Pritchard (For Now)
+ *
+ *         We need to decide what to do here.
+ *
+ *         I think this should be an enum, or something else if we don't want
+ *         the choices to be hard coded. This is because I assume that this will
+ *         be for purely for sorting means.
+ */
+public class EventType {
+
+	public String name;
+	public String description;
+	// private List<Event> EventsOfThisType // TODO not sure if this is what we want
+	// to do
+}
