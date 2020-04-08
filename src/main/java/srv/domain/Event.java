4c0ea66481811e33ba7c742614062a3ea1a8867a
AJPritchard <aj_pritch@hotmail.com>
2020-04-08 10:02:26 -0700

    Basic Event class

---
+++ src/main/java/srv/domain/Event.java
@@ -0,0 +1,60 @@
+package srv.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author AJ Pritchard
+ *
+ *         This class should hold various events. Each event should be held by
+ *         its pet and should also hold its pet.
+ *         
+ *         Importantly Event also holds and controls a list of servants
+ */
+public class Event {
+
+	private String name;
+	private String description;
+	private String time; // TODO Make this the appropriate object
+	private String date; // TODO Make this the appropriate object
+	private List<User> servants; // TODO Holds onto its servants
+	private Pet organziation;
+
+	public Event(String name, String description, String time, String date, Pet organization) {
+		this.name = name;
+		this.description = description;
+		this.time = time;
+		this.date = date;
+		this.organziation = organization;
+
+		servants = new ArrayList<User>();
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public Pet getOrganization() {
+		return organziation;
+	}
+	
+	public List<User> getServants(){
+		return servants;
+	}
+	
+	public void addServant(User servant) {
+		servants.add(servant);
+	}
+}
