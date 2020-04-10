deb1c35bda67657e4674b4a1c9c93c78807a5fc0
Emma Driscoll <edriscoll18@austincollege.edu>
2020-04-10 12:29:13 -0500

    variables added for serviceGroups

--- src/main/java/srv/domain/ServiceGroup.java
+++ src/main/java/srv/domain/ServiceGroup.java
@@ -1,5 +1,27 @@
 package srv.domain;
 
+/**
+ * @author Emma Driscoll
+ * This class is a data holder for Service groups info. 
+ *
+ */
 public class ServiceGroup {
+	
+	private String name;
+	private String groupPres;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGroupPres() {
+		return groupPres;
+	}
+	public void setGroupPres(String groupPres) {
+		this.groupPres = groupPres;
+	}
+	
 
 }
