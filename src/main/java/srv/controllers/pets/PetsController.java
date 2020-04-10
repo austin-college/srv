a9f95d97c20629d25e8478b2b2e8aa0ff1ef4242
Lydia House <lhouse18@austincollege.edu>
2020-04-09 21:21:22 -0500

    added html file and controller for pets list page
    added initial pets list page

---
+++ src/main/java/srv/controllers/pets/PetsController.java
@@ -0,0 +1,35 @@
+package srv.controllers.pets;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PetsController {
+	 /**
+	    * List action displays the list of pets page. See listPets.html template
+	    * 
+	    * @param request
+	    * @param response
+	    * @return
+	    * 
+	    * @author lahouse
+	    */
+	   @GetMapping("/list")
+	   public ModelAndView listAction(HttpServletRequest request, HttpServletResponse response) {
+		   
+		   ModelAndView mav = new ModelAndView("pets/listPets");
+		   
+		   
+		   
+		   return mav;
+	   }
+}
+
+
+	  
