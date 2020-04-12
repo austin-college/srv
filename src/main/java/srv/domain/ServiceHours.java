d4bdbef3a3afb2f01680474ede3a9a2e3d44d81f
Min <mshim18@ausitncollege.edu>
2020-04-11 21:01:16 -0500

    ServiceHours.java update
    Added basic variables, constructor, and getters/setters

--- src/main/java/srv/domain/ServiceHours.java
+++ src/main/java/srv/domain/ServiceHours.java
@@ -1,16 +1,77 @@
 package srv.domain;
 
 /**
- * @author 
+ * @author Min Shim
  *
- *	TODO
+ *	This class is a data holder for Service hours info. 
+ *	ServiceHours will include basic informations such as:
+ *	Associated Pet, Servant(or Board Member), Event, and hours served.
  *
- *	What do we need from ServiceHours?
- *	Is this going to keep track of the hours served from a single service opportunity?
- *	Is this going to keep track of all service opportunities and thus has the total hours served through that?
- *	Is this going to just have an int of total hours served?
+ *	Initially this class is keeping track of single event, 
+ *	but later it can be turned into a class tracking of all service opportunities (I need to hear more opinions about this) 
  */
 public class ServiceHours {
 
+	private Pet servedPet; // Associated Pet
+	private User servant; // Servant worked for Pet
+	private Event eventName; // Event held for service
+	private double hours;	// Hours served
 
+	
+	/**
+	 * Constructor for ServiceHours
+	 * @param servedPet
+	 * @param servant
+	 * @param event
+	 * @param hours
+	 */
+	public ServiceHours(Pet servedPet, User servant, Event eventName, double hours) {
+		super();
+		this.servedPet = servedPet;
+		this.servant = servant;
+		this.eventName = eventName;
+		this.hours = hours;
+	}
+
+
+	public Pet getServedPet() {
+		return servedPet;
+	}
+
+
+	public void setServedPet(Pet servedPet) {
+		this.servedPet = servedPet;
+	}
+
+
+	public User getServant() {
+		return servant;
+	}
+
+
+	public void setServant(User servant) {
+		this.servant = servant;
+	}
+
+
+	public Event getEventName() {
+		return eventName;
+	}
+
+
+	public void setEventName(Event eventName) {
+		this.eventName = eventName;
+	}
+
+
+	public double getHours() {
+		return hours;
+	}
+
+
+	public void setHours(double hours) {
+		this.hours = hours;
+	}
+	
+	
 }
