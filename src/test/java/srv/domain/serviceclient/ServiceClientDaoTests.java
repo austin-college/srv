d6b7e6f767e00d3f6d01bea8c9fbbbc83c1fab89
Lydia House <lhouse18@austincollege.edu>
2020-04-15 18:29:06 -0500

    added changes to tests for Service Client Dao

--- src/test/java/srv/domain/serviceclient/ServiceClientDaoTests.java
+++ src/test/java/srv/domain/serviceclient/ServiceClientDaoTests.java
@@ -1,5 +1,57 @@
 package srv.domain.serviceclient;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import srv.domain.serviceclient.JdbcServiceClientDao;
+import srv.domain.serviceclient.JdbcTemplateServiceClientDao;
+import srv.domain.serviceclient.ServiceClient;
+import srv.domain.serviceclient.ServiceClientDao;
+
 public class ServiceClientDaoTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testListAll_whenUsingJdbc() throws Exception {
+		ServiceClientDao dao = new JdbcServiceClientDao();
+
+		
+		List<ServiceClient> clients = dao.listAll();
+		
+		assertEquals(2, clients.size());
+		
+		ServiceClient sc1 = clients.get(0);
+	
+		
+		assertEquals(1, sc1.getScid());
+
+		
+		assertEquals("Work pl0x", sc1.getTitle());
+
+		
+	}
+
+	
+	
+	@Test
+	void testGetById_whenUsingJdbc() throws Exception {
+		ServiceClientDao dao = new JdbcServiceClientDao();
+
+		
+		ServiceClient sc1 = dao.fetchClientId(1);
+		
+		assertEquals(1, sc1.getScid());
+		
+		assertEquals("Work pl0x", sc1.getTitle());
+		
+	}
+
 
 }
