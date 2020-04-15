b37213b47de5cf09358388bde20eef72ba084b6a
HunterCouturier <hcouturier18@austincollege.edu>
2020-04-15 15:48:10 -0500

    implemented ServantUser class
    LogHours method is a stub for now, and will be implemeted down the line when we have something to approve hours or reject them

--- src/main/java/srv/domain/ServantUser.java
+++ src/main/java/srv/domain/ServantUser.java
@@ -1,9 +1,42 @@
 package srv.domain;
 
+/**
+ * This is class is an extension of the User class, and represents
+ * the majority of our User-base. Each Servant User will have a classification
+ * on creation, and their total hours served will be set to 0 at first as well
+ * 
+ * We will leave the affiliation to be set manually by the user
+ * 
+ * @author hunter
+ *
+ */
 public class ServantUser extends User {
+	
+	private char classification;
+	private ServiceGroup affiliation;
+	private double totalHoursServed;
+	//TODO private EventType favoriteTypes; this is on the data model but I'm not sure if its supposed to be an event type
 
-	public ServantUser(String userID, String password) {
+	public ServantUser(String userID, String password, char classification) {
 		super(userID, password);
+		this.classification = classification;
+		totalHoursServed = 0;
 	}
+	
+	/**
+	 * this method on call will collect a ServiceHours class,
+	 * and from this class will we will send the data to be approved
+	 * 
+	 * as of now, this method is a stub and is unimplemented
+	 * @param submittedHours
+	 * @return
+	 */
+	public ServiceHours logHours(ServiceHours submittedHours) {
+		
+		return submittedHours;
+	}
+	
+	
+	
 
 }
