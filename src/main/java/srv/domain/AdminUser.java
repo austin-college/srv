2ed545a1b14c1a2b55cfa6727eb7131542d4dcec
HunterCouturier <hcouturier18@austincollege.edu>
2020-04-17 01:11:30 -0500

    Created an unimplemented AdminUser

---
+++ src/main/java/srv/domain/AdminUser.java
@@ -0,0 +1,21 @@
+package srv.domain;
+
+
+/**
+ * An instance of this class represents our Admins for the system,
+ * they are an advanced user type that a great deal of control over our system
+ * 
+ * @author hunter
+ * 
+ *
+ */
+public class AdminUser extends User {
+
+	public AdminUser(String userID, String password) {
+		super(userID, password);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
