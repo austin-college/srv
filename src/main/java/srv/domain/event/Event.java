85e5310442d4443619f5ebeeb07dc37047d71333
Catalina Canizalez <ccanizalez18@austincollege.edu>
2020-04-17 11:58:33 -0500

    matched EventType and Event

--- src/main/java/srv/domain/event/Event.java
+++ src/main/java/srv/domain/event/Event.java
@@ -8,36 +8,34 @@ import srv.domain.serviceClient.ServiceClient;
 import srv.domain.user.User;
 
 /**
- * @author Segun Adekunle 
+ * @author Segun Adekunle
  *
  *         This class should hold various events. Each event should be held by
  *         its pet and should also hold its pet.
- *         
+ * 
  *         Importantly Event also holds and controls a list of servants
  */
 public class Event {
-	
-	
-	//private String title; //I'm not sure each event will have title
-	private String addr;
-	private Contact contact;
-	private String date;
-	private EventType type;
-	private boolean continous;
-	private int volunteersNumber;
-	private ServiceClient eventPet;
-	private ArrayList<User> servantList;
-	
-	
+
+	// private String title; //I'm not sure each event will have title
+	private String addr; //address of location
+	private Contact contact; // contact info
+	private String date; //date of event
+	private EventType type; // event type
+	private boolean continous;// if event repeats??
+	private int volunteersNumber; // 3 of volunteers needed
+	private ServiceClient pet; // pet associated with event
+	private ArrayList<User> servantList; //list of servants signed up for the event
+
 	public Event() {
 		super();
-	
+
 	}
-	
+
 	public Event(String date, ServiceClient eventPet, EventType type) {
 		super();
 		this.date = date;
-		this.eventPet = eventPet;
+		this.pet = eventPet;
 		this.type = type;
 	}
 
@@ -45,16 +43,13 @@ public class Event {
 		return addr;
 	}
 
-
 	public void setAddr(String addr) {
 		this.addr = addr;
 	}
 
-
 	public Contact getContact() {
 		return contact;
 	}
-
 
 	public void setContact(Contact contact) {
 		this.contact = contact;
@@ -63,27 +58,33 @@ public class Event {
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public ServiceClient getEventPet() {
-		return eventPet;
+		return pet;
 	}
+
 	public void setEventPet(ServiceClient eventPet) {
-		this.eventPet = eventPet;
+		this.pet = eventPet;
 	}
+
 	public EventType getType() {
 		return type;
 	}
+
 	public void setType(EventType type) {
 		this.type = type;
 	}
+
 	public ArrayList<User> getServantList() {
 		return servantList;
 	}
+
 	public void setServantList(ArrayList<User> servantList) {
 		this.servantList = servantList;
 	}
-	
-	
+
 }
--- src/main/java/srv/domain/event/EventType.java
+++ src/main/java/srv/domain/event/EventType.java
@@ -5,48 +5,41 @@ package srv.domain.event;
  * 
  * @author Min Shim
  *
- *			[AJ's comment] 
- *         We need to decide what to do here.
- *         I think this should be an enum, or something else if we don't want
- *         the choices to be hard coded. This is because I assume that this will
- *         be for purely for sorting means.
- *     	
- *     		[Min's comment]
- *     		This class is a data holder, mainly for sorting purpose.
- *     		Initially this class only have eventType input, and a method that
- *     		returns True if the event is AC and False if the event is non AC event.
+ *         [AJ's comment] We need to decide what to do here. I think this should
+ *         be an enum, or something else if we don't want the choices to be hard
+ *         coded. This is because I assume that this will be for purely for
+ *         sorting means.
+ * 
+ *         [Min's comment] This class is a data holder, mainly for sorting
+ *         purpose. Initially this class only have eventType input, and a method
+ *         that returns True if the event is AC and False if the event is non AC
+ *         event.
  */
 public class EventType {
 
-	private String eventType; //eventType variable should be "AC" or "Adhoc"
-	
-	// constructor for EveentType
-	public EventType(String eventType) {
+	private String name;
+	private String description;
+
+	public EventType(String name, String description) {
 		super();
-		this.eventType = eventType;
-	}
-	
-	/**
-	 * This method will tell if the event type is AC or Adhoc(non-AC events)
-	 * @param eventType
-	 * @return if eventType is AC return True, if everntType is Adhoc return False
-	 */
-	public boolean isACEvent(String eventType) {
-		if(this.eventType == "AC") {
-			return true;
-		} else {
-			return false;
-		}
+		this.name = name;
+		this.description = description;
 	}
 
-	public String getEventType() {
-		return eventType;
+	public String getName() {
+		return name;
 	}
 
-	public void setEventType(String eventType) {
-		this.eventType = eventType;
-	} 
-	
-	
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
