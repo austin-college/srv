9be7fb40ecb8c6c456e2c5ef70c8d0a3473cdedc
HunterCouturier <hcouturier18@austincollege.edu>
2020-04-17 11:29:00 -0500

    added a BoardMemberUser Domain Object

---
+++ src/main/java/srv/domain/BoardMemberUser.java
@@ -0,0 +1,10 @@
+package srv.domain;
+
+public class BoardMemberUser extends ServantUser {
+
+	public BoardMemberUser(String userID, String password, char classification) {
+		super(userID, password, classification);
+		// TODO Auto-generated constructor stub
+	}
+
+}
