ac0600d9a74a71c5dd6e8b21da3cbfbc5983df03
conormackey <conor.mackey2@gmail.com>
2020-04-19 17:20:01 -0500

    making dummy home controller

---
+++ src/main/java/srv/controllers/home/HomeController.java
@@ -0,0 +1,23 @@
+package srv.controllers.home;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * 
+ * @author Conor Mackey
+ *
+ */
+
+@Controller
+public class HomeController {
+	
+
+}
