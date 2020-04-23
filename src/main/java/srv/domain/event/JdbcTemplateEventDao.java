39790ec7579178af888e01c301d057121f84e38e
Michael Higgs <mhiggs@austincollege.edu>
2020-04-22 21:18:41 -0500

    fixed compiler error
    but much fixing is needed.

--- src/main/java/srv/domain/event/JdbcTemplateEventDao.java
+++ src/main/java/srv/domain/event/JdbcTemplateEventDao.java
@@ -8,50 +8,27 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import srv.domain.JdbcTemplateAbstractDao;
 import srv.domain.contact.Contact;
+import srv.domain.contact.JdbcTemplateContactDao;
 import srv.domain.user.JdbcTemplateUserDao;
 import srv.domain.user.User;
 
-public class JdbcTemplateEventDao implements EventDao {
+@ComponentScan("srv.config")
+public class JdbcTemplateEventDao extends JdbcTemplateAbstractDao implements EventDao {
 
 	private static Logger log = LoggerFactory.getLogger(JdbcTemplateUserDao.class);
 
-	private DataSource dataSource;
-
-	private JdbcTemplate jdbcTemplate;
-
-	public DataSource getDataSource() {
-		if (dataSource == null)
-			dataSource = h2DataSource();
-
-		return dataSource;
-	}
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
-	public DataSource h2DataSource() {
-		// script to create
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("data.sql") // person table
-				.build();
-	}
-
-	public JdbcTemplate getJdbcTemplate() {
-		if (jdbcTemplate == null)
-			jdbcTemplate = new JdbcTemplate(getDataSource());
-
-		return jdbcTemplate;
-	}
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+	@Autowired
+	private JdbcTemplateContactDao contactDao;
+	
 
 	public JdbcTemplateEventDao() {
 		super();
@@ -69,7 +46,7 @@ public class JdbcTemplateEventDao implements EventDao {
 
 		// update executes the sql script that is in its paramaters. The sequel script
 		// inserts a new user into the database
-		int rc = jdbcTemplate.update(
+		int rc = getJdbcTemplate().update(
 				"insert into events (title, address, contactId, dateOf, eventType, continuous, volunteersNeeded, organizationId, participantsListId) values ( '"
 						+ title + "', '" + addr + "', '" + date + "', " + cid + ", '" + eventType + "', " + continuous
 						+ ", " + volunteersNeeded + ", " + organizationId
@@ -86,7 +63,7 @@ public class JdbcTemplateEventDao implements EventDao {
 				"SELECT eventId, title, contactId, dateOf, eventType, continuous, volunteersNeeded, organizationId, participantsListId FROM users WHERE username = '%s'",
 				title), new EventRowMapper());
 
-		jdbcTemplate.update("CREATE TABLE participants" + results.getEid() + " (participantNum integer auto_increment,"
+		getJdbcTemplate().update("CREATE TABLE participants" + results.getEid() + " (participantNum integer auto_increment,"
 				+ "	userId int, numHoursServed double, primary key (participantNum),"
 				+ "	foreign key (userId) references users(userId) on delete set NULL);");
 
@@ -134,13 +111,33 @@ public class JdbcTemplateEventDao implements EventDao {
 		public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
 
 			Event us = new Event();
-			us.setEid(rs.getInt("eventId")).setTitle(rs.getString("title")).setAddress(rs.getString("address"))
-					.setContactId(rs.getInt("contactId")).setDate(rs.getString("dateOf"))
-					.setType(rs.getString("eventType")).setContinuous(rs.getBoolean("continuous"))
-					.setVolunteersNeeded(rs.getInt("volunteersNeeded"))
-					.setSetServiceClientId(rs.getInt("organizationId"))
-					.setParticipantsListId(rs.getInt("participantsListId"));
-
+			
+			try {
+				us.setEid(rs.getInt("eventId"))
+				  .setTitle(rs.getString("title"))
+				  .setAddress(rs.getString("address"))
+				
+				  .setContact(contactDao.fetchContactById(rs.getInt("contactId")))
+						  
+				  .setDate(rs.getString("dateOf"))
+				  
+				  //.setType(rs.getString("eventType"))  must fix doa for event type first
+				  
+				  // .setContinuous(rs.getBoolean("continuous"))  use the right param
+				  
+				  .setVolunteersNeeded(rs.getInt("volunteersNeeded"))
+				  // .setSetServiceClientId(rs.getInt("organizationId")) check param types
+				  
+				  // .setParticipantsListId(rs.getInt("participantsListId"));
+				  ;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			return us;
 		}
 	}
