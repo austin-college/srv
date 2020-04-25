a78e33891f1c2c473a034253f63dd76a0379e16a
Kevin M <ygopotato@gmail.com>
2020-04-25 12:48:35 -0500

    Board Member controller
    Tied to boardMember.html template

---
+++ src/main/java/srv/controllers/boardMember/BoardMemberController.java
@@ -0,0 +1,40 @@
+package srv.controllers.hours;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * This is the algorithm that prepares the response
+ * for the board member home page.
+ *
+ */
+
+@Controller
+public class BoardMemberController {
+
+	private static Logger log = LoggerFactory.getLogger(BoardMemberController.class);
+
+	/**
+	 * Maps boardMember.html template to /boardMember
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@GetMapping("/boardMember")
+	public ModelAndView splashAction(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView mav = new ModelAndView("home/boardMember");
+
+		return mav;
+	}
+	
+}
