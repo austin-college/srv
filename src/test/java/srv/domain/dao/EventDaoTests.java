44901e11493763ab25efd86854925324e3dac089
AJPritchard <aj_pritch@hotmail.com>
2020-04-26 15:40:06 -0700

    Eventdao bean createdddddddddddd

--- src/test/java/srv/domain/dao/EventDaoTests.java
+++ src/test/java/srv/domain/dao/EventDaoTests.java
@@ -3,9 +3,22 @@ package srv.domain.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import srv.domain.event.EventDao;
+
+@RunWith(SpringRunner.class)
+@JdbcTest
+@ComponentScan("srv.config")
 class EventDaoTests {
 
+	@Autowired
+	EventDao dao; 
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
