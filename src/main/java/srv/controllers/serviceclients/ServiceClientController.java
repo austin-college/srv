6176c6f33df78a3f9c7168cf278692fe54e754e5
Lydia House <lhouse18@austincollege.edu>
2020-04-27 22:14:36 -0500

    working on making delete button for pets live
    refresh page to see deletion

--- src/main/java/srv/controllers/serviceclients/ServiceClientController.java
+++ src/main/java/srv/controllers/serviceclients/ServiceClientController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -57,6 +58,44 @@ public class ServiceClientController {
 		   
 		   return mav;
 	   }
+
+	   /**
+	    *  Ajax action that renders a new page removing the selected service client from the table.
+	    * 
+	    * 
+	    * @param request
+	    * @param response
+	    * @return MAV of the deleted service client row of the table
+	    */
+	   @PostMapping("/ajax/delServiceClient")
+	   public ModelAndView ajaxServiceClientDelete(HttpServletRequest request, HttpServletResponse response) {
+
+		   response.setContentType("text/html");
+
+		   int id = Integer.parseInt(request.getParameter("ID")); 
+		   
+			/*
+			 * Prepare and render the response of the template's model for the HTTP response
+			 */
+		   ModelAndView mav = new ModelAndView("/serviceclients/ajax_delServiceClient");
+
+		   try {
+			   
+			   doa.delete(id);   
+			   //return id;
+			   mav.addObject("scid", id);
+			   
+		   } catch (Exception e) {
+			   System.err.println("\n\n ERROR ");
+			   System.err.println(e.getMessage());
+			   
+		//	   return -1;
+		   }
+		   
+		   return mav;		   
+	   }
+	   
+	   
 	   
 	   
 	   @GetMapping("/test/sc")
