63c73553ce5c898400dd5c742699a5a654616da1
Min <mshim18@ausitncollege.edu>
2020-04-29 08:23:22 -0500

    Created ServiceGroupDao

---
+++ src/main/java/srv/domain/serviceGroup/ServiceGroupDao.java
@@ -0,0 +1,25 @@
+package srv.domain.serviceGroup;
+
+import java.util.List;
+
+import srv.domain.contact.Contact;
+
+/**
+ * Data Access Object Interface for EvenetType.java
+ * 
+ * @author Min Shim
+ *
+ */
+public interface ServiceGroupDao {
+
+	public List<ServiceGroup> listAll() throws Exception;
+	
+	// Note that cid is the contactId, same as contactInfo from ServiceGroup.java
+	public ServiceGroup create(String shortName, String title, int cid) throws Exception;
+	
+	public void delete(int sgid) throws Exception;
+	
+	public void update(int sgid, String shortName, String title, int cid) throws Exception;
+	
+	public ServiceGroup fetchServiceGroupById(int sgid) throws Exception;
+}
