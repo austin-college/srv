4f65acd7cd28161399f7c163bad2f7d64f4cc724
Kevin M <ygopotato@gmail.com>
2020-05-02 19:48:25 -0500

    Attach bmhrslistserv bean to controller

--- src/main/java/srv/controllers/boardMember/BoardMemberController.java
+++ src/main/java/srv/controllers/boardMember/BoardMemberController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,8 @@ import srv.services.BoardMemberHoursListService;
 public class BoardMemberController {
 
 	private static Logger log = LoggerFactory.getLogger(BoardMemberController.class);
+	
+	@Autowired
 	BoardMemberHoursListService bmHrListSrv = new BoardMemberHoursListService();
 	
 
