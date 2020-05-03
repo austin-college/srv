156e4594e0e678b1b403e6788212a9df7ff16208
Michael Higgs <mhiggs@austincollege.edu>
2020-05-03 13:20:46 -0500

    Added selenium test example

---
+++ src/test/java/srv/SplashTest.java
@@ -0,0 +1,26 @@
+package srv;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SplashTest extends SeleniumTest {
+	
+    @Test
+    public void testTest() throws Exception {
+        driver.get(base + "/splash");
+    }
+}
