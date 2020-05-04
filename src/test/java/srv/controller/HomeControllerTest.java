1420f052700389a358e456659c5b40f0df848e7f
HunterCouturier <hcouturier18@austincollege.edu>
2020-05-04 17:25:36 -0500

    Mocking a log in
    with the use of a mock user, we should be able to log into the site without having to bypass security for tests

--- src/test/java/srv/controller/HomeControllerTest.java
+++ src/test/java/srv/controller/HomeControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import srv.controllers.home.HomeController;
@@ -26,20 +27,11 @@ public class HomeControllerTest {
 
 
 	@Test
+	@WithMockUser(username = "user", password = "user")
 	public void basicHtmlPageTest() throws Exception {
 		boolean pageLoadedSucessfully = false;
+		pageLoadedSucessfully = UserUtil.userIsServant();
 
-
-		try {
-			
-			mvc.perform(get("/login?userid=user? password=user").param("userid=user", "password=use").contentType(MediaType.TEXT_HTML))
-			.andExpect(status().isOk());
-			
-			pageLoadedSucessfully = UserUtil.userIsServant();
-			
-		}  finally {
-			
-		}
 		
 		assertTrue(pageLoadedSucessfully);
 		
