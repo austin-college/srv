69cce98964e0fb4c01a09e42321862ba86aa2142
AJPritchard <aj_pritch@hotmail.com>
2020-05-03 23:44:51 -0700

    Service Hours Controller Test Done

--- src/main/java/srv/controllers/hours/HoursController.java
+++ src/main/java/srv/controllers/hours/HoursController.java
@@ -1,6 +1,8 @@
 package srv.controllers.hours;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import srv.domain.event.Event;
+import srv.domain.reason.Reason;
+import srv.domain.serviceHours.ServiceHours;
 import srv.services.ServiceHoursService;
 
 /**
@@ -133,4 +137,37 @@ public class HoursController {
 
 		return mav;
 	}
+	
+	/** 
+	    * This request handle renders an entire page useful for testing only.   This
+	    * is not part of our actual site.
+	    */
+	   @GetMapping("/test/hours")
+	   public ModelAndView handleReasonRequest(HttpServletRequest request, HttpServletResponse response) {
+		   
+		   ModelAndView mav = new ModelAndView("test/hoursTestView");
+
+
+		   try {
+			   
+			int cnt = hrSvc.listHours().size();
+			
+			mav.addObject("count",cnt);
+			
+			List<ServiceHours> myHours = hrSvc.listHours();
+			
+			mav.addObject("serviceHours", myHours );
+			
+			} catch (Exception e) {
+				
+				System.err.println("\n\n ERROR ");
+				System.err.println(e.getMessage());
+				
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		   
+		   
+		   return mav;
+	   }
 }
