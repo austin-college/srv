91e4505487d2800493a40b1beb8bb5d6c191d25c
conormackey <conor.mackey2@gmail.com>
2020-05-06 19:14:31 -0500

    stats on viewHours now draw from database
    just using the data already in there since there aren't different servant users yet

--- src/main/java/srv/services/ServiceHoursService.java
+++ src/main/java/srv/services/ServiceHoursService.java
@@ -124,8 +124,39 @@ public class ServiceHoursService {
 		for(int i = 0; i < hours.size(); i++) {
 			avg += hours.get(i).getHours();
 		}
-		System.out.println(hours.size() + ", " + avg);
 		avg = avg / hours.size();
+		
+		
+		return avg;
+	}
+	
+	public double getTermTot(List<ServiceHours> hours) {
+		double avg = 0;
+		
+		//before this you would make a new list with the dates being from the last term
+		for(int i = 0; i < hours.size(); i++) {
+			avg += hours.get(i).getHours();
+		}
+		avg = avg / hours.size();
+		
+		
+		return avg * 2;
+	}
+	
+	public int getTotOrgs(List<ServiceHours> hours) {
+		//before this there should be loop that weeds out all the duplicate orgs
+		int orgs = hours.size();
+		return orgs;
+	}
+	
+	public double getAvgPerMo(List<ServiceHours> hours) {
+		double avg = 0;
+		
+		//before this you would make a new list with the dates being from the last year
+		for(int i = 0; i < hours.size(); i++) {
+			avg += hours.get(i).getHours();
+		}
+		avg = avg / 12;
 		
 		
 		return avg;
