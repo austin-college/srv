872881d08e2178ab76012418efe2c65024afa31a
Michael Higgs <mhiggs@austincollege.edu>
2020-05-11 14:47:55 -0500

    running selenium tests headless
    Running headless (no browser window displayed) to increase speed of integration tests.   No waiting for graphics to render.

--- src/test/java/srv/SeleniumTest.java
+++ src/test/java/srv/SeleniumTest.java
@@ -39,7 +39,7 @@ public abstract class SeleniumTest {
     	
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
