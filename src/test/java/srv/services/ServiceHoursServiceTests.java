c873193db8500bc65e7a0753c655923b8df8fb03
fancynine9 <skhaled18@austincollege.edu>
2020-06-06 21:03:23 -0500

    created junit test for ServiceHoursService

---
+++ src/test/java/srv/services/ServiceHoursServiceTests.java
@@ -0,0 +1,14 @@
+package srv.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServiceHoursServiceTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
