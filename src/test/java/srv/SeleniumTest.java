8a71d919b0ed32eaba6806ff75dcd0fdef1211db
HunterCouturier <hcouturier18@austincollege.edu>
2020-06-15 21:44:51 -0500

    created a method in SeleniumTest
    added wait by ID to the test

--- src/test/java/srv/SeleniumTest.java
+++ src/test/java/srv/SeleniumTest.java
@@ -7,10 +7,13 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,78 +21,111 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SeleniumTest {
-	
+
 	public static final String WINDRIVERPATH = "src/main/resources/chromedriver.exe";
 	public static final String MACDRIVERPATH = "src/main/resources/chromedriver";
 
-    @LocalServerPort
-    protected int port;
-    
-    protected String base;
-    
-    
-    protected static ChromeDriverService service;
-    
-    protected WebDriver driver;
+	@LocalServerPort
+	protected int port;
 
-    
-    @Before
-    public void setUp() throws Exception {
-    	
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
-        options.addArguments("--headless");
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        
-    	
-        driver = new ChromeDriver(service,options);
-        
-        this.base = "http://localhost:" + port+"/srv";
-        System.err.println(base);
-    }
-    
+	protected String base;
 
 
-    @BeforeClass
-    public static void createAndStartService() throws Exception {
-    	String path;
-    	System.err.println(System.getProperty("os.name"));
-      if(System.getProperty("os.name").toLowerCase().contains("win")) {
-    	  path = WINDRIVERPATH;
+	protected static ChromeDriverService service;
 
-      } else {
-    	  path = MACDRIVERPATH;
-    	  
-      }
-      System.setProperty("webdriver.chrome.driver", path);
-
-    	
-    	      
-      service = new ChromeDriverService.Builder()
-          .usingDriverExecutable(new File(path))
-          .usingAnyFreePort()
-          .build();
-      
-      
-      service.start();
-    }
-
-    @AfterClass
-    public static void stopService() {
-      service.stop();
-    }
+	protected WebDriver driver;
 
 
-    @After
-    public void quitDriver() {
-      driver.quit();
-    }
-    
-    
+	@Before
+	public void setUp() throws Exception {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+		options.addArguments("--headless");
+		options.setExperimentalOption("useAutomationExtension", false);
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+
+
+		driver = new ChromeDriver(service,options);
+
+		this.base = "http://localhost:" + port+"/srv";
+		System.err.println(base);
+	}
+
+
+
+	@BeforeClass
+	public static void createAndStartService() throws Exception {
+		String path;
+		System.err.println(System.getProperty("os.name"));
+		if(System.getProperty("os.name").toLowerCase().contains("win")) {
+			path = WINDRIVERPATH;
+
+		} else {
+			path = MACDRIVERPATH;
+
+		}
+		System.setProperty("webdriver.chrome.driver", path);
+
+
+
+		service = new ChromeDriverService.Builder()
+				.usingDriverExecutable(new File(path))
+				.usingAnyFreePort()
+				.build();
+
+
+		service.start();
+	}
+
+	@AfterClass
+	public static void stopService() {
+		service.stop();
+	}
+
+
+	@After
+	public void quitDriver() {
+		driver.quit();
+	}
+
+	/**
+	 * On run, this method will click on the button that makes the dialog box visible
+	 * and will cause the test system to wait until the dialog box is fully visible before allowing
+	 * the code following it to run
+	 * 
+	 * 
+	 * @param driver
+	 * @param waitTime
+	 * @param clickedItemId
+	 * @param dialogId
+	 */
+	private void clickAndWaitForDialogById(WebDriver driver, int waitTime, String clickedItemId, String dialogId) {
+
+		final String currentUrl = driver.getCurrentUrl();
+		driver.findElement(By.id(clickedItemId)).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, waitTime);
+
+		wait.until(new ExpectedCondition<Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver driver) {
+
+				Boolean isPresent;
+				isPresent = driver.findElement(By.id(dialogId)).isDisplayed();
+
+				return isPresent;
+			}
+		}
+				);
+
+	}
+
+
 
 }
