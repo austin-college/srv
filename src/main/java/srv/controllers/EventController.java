a0d04cd113942b52d10e90c0e193287490387d49
Catalina Canizalez <ccanizalez18@austincollege.edu>
2020-07-09 23:32:17 -0500

    combo box structure added but not functional yet

--- src/main/java/srv/controllers/EventController.java
+++ src/main/java/srv/controllers/EventController.java
@@ -466,7 +466,7 @@ public class EventController {
     		log.debug("creating new event type={}", etid);
     		
 			Event newev = eventService.createEventOfType(etid);
-			mav.addObject("service client", newev.getType().getDefClient());
+			mav.addObject("service client", newev.getServiceClient());
 			
 			// return the event id of the newly created object
 		    return new ResponseEntity<Integer>(newev.getEid(), HttpStatus.OK);
