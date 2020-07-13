ad7d3023025e0a589fc8726751421d9cc157ad6e
HunterCouturier <hcouturier18@austincollege.edu>
2020-07-13 13:33:25 -0500

    Fixed Selenium Spotlight Page Test
    Test will now submit a image and test it

--- src/test/java/srv/admin/AdminManagesSpotlight.java
+++ src/test/java/srv/admin/AdminManagesSpotlight.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import srv.SeleniumTest;
@@ -44,54 +45,59 @@ public class AdminManagesSpotlight extends SeleniumTest {
 
 		//makes sure we are on the admin's home page
 		assertEquals(base+"/home/admin?userid=admin", driver.getCurrentUrl());
-		
+
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		jse.executeScript("window.scrollBy(0,250)", "");
-		
+
 		//clicks on the manage spotlight button
 		WebElement link = driver.findElement(By.xpath("//div/a[@href='/srv/spotlight']")); 
 		link.click();
-		
+
 		//makes sure we are on the Manage Spotlight page
 		assertEquals(base+"/spotlight", driver.getCurrentUrl());
-		
-//		//selects browse button for adding a new image and submits an image
-//		link = driver.findElement(By.xpath("//div/input"));
-//		//TODO make this work for everyone, right now its just me
-//		link.sendKeys("C:\\Users\\hunte\\OneDrive\\Desktop\\CS 380 Workspace\\srv\\src\\main\\resources\\static\\images\\defaultProfilePicture.png");
-//		
-		
+
+		ClassPathResource imgFile = new ClassPathResource("/static/images/defaultProfilePicture.png", this.getClass().getClassLoader());
+
+		String absolutePath = imgFile.getFile().getAbsolutePath(); 
+
+		//selects browse button for adding a new image and submits an image
+		link = driver.findElement(By.xpath("//div/input"));
+		//TODO make this work for everyone, right now its just me
+		link.sendKeys(absolutePath);
+
+
 		jse.executeScript("window.scrollBy(0,400)", "");
 		Thread.sleep(1000);
-//		//submits the image
-//		link = driver.findElement(By.xpath("//form[@action='/srv/spotlight/img/uploadFile']/button"));
-//		link.click();
-		
+
+		//submits the image
+		link = driver.findElement(By.xpath("//form[@action='/srv/spotlight/img/uploadFile']/button"));
+		link.click();
+
 		//clicks on the edit text, text area and writes text
 		link = driver.findElement(By.xpath("//div[@id='editor']/div"));
 		link.clear();
 		link.sendKeys("TEST OF THE SYSTEM");
-		
+
 		//clicks on the submit button for text
 		link = driver.findElement(By.xpath("//form[@id='frmText']/button"));
 		link.click();
-		
+
 		//logs out of the system to return to splash page
 		logout();
-		
+
 		//checks that the image and text have been changed
-		
+
 		//text
 		assertEquals("TEST OF THE SYSTEM", driver.findElement(By.xpath("//div[@id='spotlightDesc']/p")).getText());
-		
+
 		//image
 		//TODO figure out how to make sure the image was uploaded
 		assertEquals("","");
-		
-		
-		
-		
-		
+
+
+
+
+
 	}
 
 }
